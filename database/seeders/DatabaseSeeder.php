172a2692132b9f16220d21d9fc0298d0ba368139
ElsmrSoufiane <soufianeasmar0@gmail.com>
2025-03-14 18:58:10 +0100

    5

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
             'description' => 'responsable du group 203',
             'login' => 'admin1',
             'password' => Hash::make('p2'),
-            'image' => 'storage/user.png',
+            'image' => 'https://www.pngall.com/wp-content/uploads/5/Profile.png',
             'created_at' => now(),
             'updated_at' => now(),
         ]);
